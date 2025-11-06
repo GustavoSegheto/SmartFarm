fca3745ffcf0b622ea2b008c68e5592da7df872c
MatheusGSMendes <matheusg.mendes.g5@gmail.com>
2025-11-06 17:00:53 -0300

    alterações Node.js

--- NodeJS/config/conexao.js
+++ NodeJS/config/conexao.js
@@ -1,6 +1,8 @@
-let mysql = require('mysql2');
+//conexao.js
 
-let con = mysql.createConnection({
+const mysql = require('mysql2');
+
+const con = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "root",
@@ -8,7 +10,9 @@ let con = mysql.createConnection({
     port: 3306
 });
 
-con.connect(function(err) {
+/*con.connect(function(err) {
     if(err) throw err;
     console.log("Connected!");
-});
+});*/
+
+module.exports = con;
